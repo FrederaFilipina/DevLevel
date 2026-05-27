2be573551753bb3a969cbc3564c1317bf2b835bb
Felipecardosodacosta <felipe.cardoso.costa96@gmail.com>
2026-05-26 22:02:34 -0300

    feat: criacao da estrutara e configuracao para desenvolvimento

---
+++ api/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express"
+
+
+
+const port = 3000
+const app = express()
+
+app.use(express.json())
+
+
+
+
+
+app.listen(port,()=> console.log("Servidor rodando...  (: "))
+
+
+
+export default app
